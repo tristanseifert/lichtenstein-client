f7ca32efb038803afca4a98abadcbb71bf0b9272
Tristan Seifert <t@tseifert.me>
2018-05-17 19:22:31 -0500

    add some testing scripts

---
+++ unexport_gpios.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo 500 > /sys/class/gpio/unexport
+echo 501 > /sys/class/gpio/unexport
+echo 502 > /sys/class/gpio/unexport
+echo 503 > /sys/class/gpio/unexport
+echo 504 > /sys/class/gpio/unexport
+echo 505 > /sys/class/gpio/unexport
+echo 506 > /sys/class/gpio/unexport
+echo 507 > /sys/class/gpio/unexport
+echo 508 > /sys/class/gpio/unexport
+echo 509 > /sys/class/gpio/unexport
+echo 510 > /sys/class/gpio/unexport
+echo 511 > /sys/class/gpio/unexport
