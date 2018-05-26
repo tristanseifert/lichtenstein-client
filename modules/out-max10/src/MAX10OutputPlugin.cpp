4fde7f0e5052f5b74e0242bfca5f901b75954f7f
Tristan Seifert <t@tseifert.me>
2018-05-25 18:43:10 -0700

    Begin working on framebuffer allocation in MAX10 plugin

--- modules/out-max10/src/MAX10OutputPlugin.cpp
+++ modules/out-max10/src/MAX10OutputPlugin.cpp
@@ -1,21 +1,94 @@
 #include "MAX10OutputPlugin.h"
 
+#include <glog/logging.h>
+#include <INIReader.h>
+
 #include <OutputFrame.h>
 
-MAX10OutputPlugin::MAX10OutputPlugin(void *romData, size_t length) : OutputPlugin(romData, length) {
+// size of each "block" in the framebuffer allocation map
+static const size_t kFBMapBlockSize = 16;
 
-}
 
-MAX10OutputPlugin::~MAX10OutputPlugin() {
-
-}
 
 /**
  * Invokes the constructor for the plugin and returns it.
  */
 OutputPlugin *MAX10OutputPlugin::create(PluginHandler *handler, void *romData, size_t length) {
-	return new MAX10OutputPlugin(romData, length);
+	return new MAX10OutputPlugin(handler, romData, length);
 }
+
+/**
+ * Initializes the output plugin, allocating memory and setting up the hardware.
+ */
+MAX10OutputPlugin::MAX10OutputPlugin(PluginHandler *_handler, void *romData, size_t length) : handler(_handler), OutputPlugin(romData, length) {
+	// get SPI settings
+	this->configureSPI();
+
+	// allocate framebuffer
+	this->allocateFramebuffer();
+
+	// reset the output chip
+	this->reset();
+}
+
+/**
+ * Configures the SPI bus.
+ */
+void MAX10OutputPlugin::configureSPI(void) {
+	INIReader *config = this->handler->getConfig();
+
+	// TODO: read from EEPROM data
+	this->spiBaud = config->GetInteger("output_max10", "baud", 2500000);
+
+	this->spiCsLine = config->GetInteger("output_max10", "cs", -1);
+	CHECK(this->spiCsLine >= 0) << "Invalid chip select line " << this->spiCsLine;
+}
+
+/**
+ * Allocates a shadow copy of the framebuffer on the board: received frames are
+ * "fit" into this buffer and will later be transferred to the chip.
+ */
+void MAX10OutputPlugin::allocateFramebuffer(void) {
+	INIReader *config = this->handler->getConfig();
+
+	// TODO: read from EEPROM data
+	this->framebufferLen = config->GetInteger("output_max10", "fbsize", 131072);
+
+	if((this->framebufferLen % kFBMapBlockSize) != 0) {
+		LOG(FATAL) << "Framebuffer length must be a multiple of "
+			<< kFBMapBlockSize << "; config specified " << this->framebufferLen;
+	}
+
+	this->framebuffer = static_cast<uint8_t *>(malloc(this->framebufferLen));
+	CHECK(this->framebuffer != nullptr) << "Couldn't allocate framebuffer!";
+
+	// allocate the framebuffer use
+	size_t blocks = this->framebufferLen / kFBMapBlockSize;
+
+	this->fbUsedMap.clear();
+	this->fbUsedMap.reserve(blocks);
+
+	for(size_t i = 0; i < blocks; i++) {
+		this->fbUsedMap.push_back(false);
+	}
+}
+
+
+
+/**
+ * Resets the hardware and de-allocates memory.
+ */
+MAX10OutputPlugin::~MAX10OutputPlugin() {
+	// reset peripheral
+	this->reset();
+
+	// de-allocate framebuffer
+	if(this->framebuffer) {
+		free(this->framebuffer);
+	}
+}
+
+
 
 const std::string MAX10OutputPlugin::name(void) {
 	return "MAX10 FPGA Output Board";
@@ -26,6 +99,15 @@ const unsigned int MAX10OutputPlugin::maxChannels(void) {
 
 int MAX10OutputPlugin::setEnabledChannels(unsigned int channels) {
 	return -1;
+}
+
+
+
+/**
+ * Resets the output chip.
+ */
+void MAX10OutputPlugin::reset(void) {
+	// TODO: implement
 }
 
 
--- modules/out-max10/src/MAX10OutputPlugin.h
+++ modules/out-max10/src/MAX10OutputPlugin.h
@@ -4,8 +4,9 @@
 #include <lichtenstein_plugin.h>
 
 #include <cstddef>
+#include <cstdint>
+
 #include <string>
-#include <vector>
 #include <queue>
 #include <bitset>
 
@@ -13,7 +14,7 @@ class OutputFrame;
 
 class MAX10OutputPlugin : public OutputPlugin {
 	public:
-		MAX10OutputPlugin(void *romData, size_t length);
+		MAX10OutputPlugin(PluginHandler *handler, void *romData, size_t length);
 		~MAX10OutputPlugin();
 
 		static OutputPlugin *create(PluginHandler *handler, void *romData, size_t length);
@@ -28,10 +29,35 @@ class MAX10OutputPlugin : public OutputPlugin {
 		virtual int outputChannels(std::bitset<32> &channels);
 
 	private:
+		void configureSPI(void);
+		void allocateFramebuffer(void);
+
+		void reset(void);
+
+	private:
 		PluginHandler *handler = nullptr;
 
 		// queue of output frames
 		std::queue<OutputFrame *> outFrames;
+
+		// SPI baud rate and chip select
+		unsigned int spiBaud = 0;
+		int spiCsLine = -1;
+
+		// framebuffer memory
+		uint8_t *framebuffer = nullptr;
+		size_t framebufferLen = 0;
+
+		/**
+		 * Bitmap of used memory in the framebuffer: each bit represents a span
+		 * of N bytes. When the bit is set (true), that span of N bytes is
+		 * in use. Index 0 refers to addres 0 in the framebuffer, 1 to N,
+		 * and so forth.
+		 *
+		 * N is defined as kFBMapBlockSize in the .cpp file. Smaller values save
+		 * host memory, but force a more coarse allocation of framebuffer memory.
+		 */
+		std::vector<bool> fbUsedMap;
 };
 
 #endif
